877da31afbbfddd03069cd1990417d08897e7066
magicdawn <784876393@qq.com>
2014-12-10 19:27:43 +0800

    config -> peerDependency

--- debug.js
+++ debug.js
@@ -1,37 +1,37 @@
 var pathFn = require('path')
 var winston = require('winston')
 var config = require('config');
-var _ = require('lodash');
 require("winston-logio")
 
 module.exports = _debug
 
 // loggers_map
 var loggers = {}
-function getLogger (namespace) {
-    if(loggers[namespace]){
+
+function getLogger(namespace) {
+    if (loggers[namespace]) {
         return loggers[namespace]
     }
 
     var logger = loggers[namespace] = new winston.Logger({
         levels: config.levels // logger 的 levels
     })
-    
+
     // // console
     // logger.add(winston.transports.Console,{
     //     level: config.console_level
     // })
 
     // file
-    logger.add(winston.transports.File,{
+    logger.add(winston.transports.File, {
         levels: config.levels,
-        level: config.file_level, 
+        level: config.file_level,
 
-        filename: pathFn.join(config.logs_dir,namespace+'.log') // 在项目主文件夹运行
+        filename: pathFn.join(config.logs_dir, namespace + '.log') // 在项目主文件夹运行
     })
 
     // log.io
-    logger.add(winston.transports.Logio,{
+    logger.add(winston.transports.Logio, {
         levels: config.levels,
         level: config.logio_level,
 
@@ -46,22 +46,23 @@ function getLogger (namespace) {
     return logger
 }
 
-function _debug (namespace) {
+function _debug(namespace) {
     //debugFactory(namespace)
 
     var logger = getLogger(namespace)
 
     // 必须配置debug level
     var debug = logger.debug;
-    
+
     // 添加 debug.debug 等配置在levels里的方法
-    _.forOwn(logger.levels,function(num,m) {
-        debug[m] = function() {
-            // debug.debug()
-            logger[m].apply(logger,arguments);
-        };
-    })
-    
+    for (var level in logger.levels) {
+        if (logger.hasOwnProperty(level)) {
+            debug[level] = function() {
+                logger[level].apply(logger, arguments);
+            };
+        }
+    }
+
     return debug
 }
 
